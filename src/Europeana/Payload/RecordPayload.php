7b7fd2c140e56324d6bb2913bdcabfcdbe2c815b
Matthias Vandermaesen <matthias@colada.be>
2015-05-01 18:36:29 +0200

    Added: basic response payload call

---
+++ src/Europeana/Payload/RecordPayload.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Europeana\Payload;
+
+use Europeana\Exception\EuropeanaException;
+
+/**
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
+class RecordPayload extends AbstractPayload
+{
+    private $recordId;
+
+    public function setRecordId($recordId)
+    {
+	$this->recordId = $recordId;
+    }
+
+    public function getRecordId()
+    {
+	return $this->recordId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethod()
+    {
+	return 'record' . $this->recordId . '.json';
+    }
+}
---
+++ src/Europeana/Payload/RecordPayloadHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Europeana\Payload;
+
+use Europeana\Payload\AbstractPayloadHandler;
+
+class RecordPayloadHandler extends AbstractPayloadHandler
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+    }
+}
