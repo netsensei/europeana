335aea63fefe7a7b96fc971f656629f93a39d416
Matthias Vandermaesen <matthias@colada.be>
2015-05-01 18:36:29 +0200

    Added: basic response payload call

---
+++ src/Europeana/Payload/RecordPayloadResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Europeana\Payload;
+
+/**
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
+class RecordPayloadResponse extends AbstractPayloadResponse
+{
+}
