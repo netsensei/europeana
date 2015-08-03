4578dd9eaa2fe2aa859760efa2daa8c7408b42ff
Matthias Vandermaesen <matthias@colada.be>
2015-08-03 08:04:12 +0200

    added: contextual EDM resources

---
+++ src/Colada/Europeana/Model/EDM/TimeSpan.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Colada\Europeana\Model\EDM;
+
+use Colada\Europeana\Model\AbstractModel;
+
+/**
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
+class TimeSpan extends AbstractModel
+{
+    // @todo Implement me!
+}
