374880eee938fdec19bf406e956813080bd8fa87
Matthias Vandermaesen <matthias@colada.be>
2015-08-04 08:18:42 +0200

    added: basic implementation of providers.json

---
+++ src/Colada/Europeana/Payload/ProvidersPayload.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Colada\Europeana\Payload;
+
+use Colada\Europeana\Exception\EuropeanaException;
+
+/**
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
+class ProvidersPayload extends AbstractPayload
+{
+    private $offset;
+
+    private $pageSize;
+
+    private $countryCode;
+
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    public function setPageSize($pageSize)
+    {
+        $this->pageSize = $pageSize;
+    }
+
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    public function setCountryCode($countryCode)
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+}
---
+++ src/Colada/Europeana/Payload/ProvidersPayloadHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Europeana API package.
+ *
+ * (c) Matthias Vandermaesen <matthias@colada.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Colada\Europeana\Payload;
+
+/**
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
+class ProvidersPayloadHandler extends AbstractPayloadHandler
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+        $payload = $this->getPayload();
+
+        if ($offset = $payload->getOffset()) {
+            $arguments[] = array('offset', $offset);
+        }
+
+        if ($pageSize = $payload->getPageSize()) {
+            $arguments[] = array('pagesize', $pageSize);
+        }
+
+        if ($countryCode = $payload->getCountryCode()) {
+            $arguments[] = array('countryCode', $countryCode);
+        }
+
+   	    return $arguments;
+    }
+}
